712b3cbbde35948fafde6b3b10d24395bce4c4b6
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-25 12:37:00 -0600

    Pass the first 4 tests.

--- functions.js
+++ functions.js
@@ -1,9 +1,15 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
 }
 
+function sayHello(name) {
+  return 'Hi there' + ' ' + name + '!';
+}
+
+//Below object module.exports exports the named functions in the object to the functions-test.js. This should be the last thing in your file. The functions that you create shold be above this.
 module.exports = {
-    addTwoNumbers,
-    // sayHello,
+    addTwoNumbers, // see line 6 in functions-test.js
+    sayHello,
     // buildCar
 }
